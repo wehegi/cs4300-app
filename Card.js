3bad5f998b5bf022b245810ba9649b51041050e3
wehegi <weh12798@uga.edu>
2023-04-16 19:49:03 -0400

    login form/routing
    Added login form and navigation buttons

--- Card.js
+++
@@ -1,6 +0,0 @@
-import './Card.css';
-const Card = (props) => {
-    const class1 = 'card ' + props.className;
-    return <div className={class1}>{props.children}</div>;
-  };
-  export default Card;
